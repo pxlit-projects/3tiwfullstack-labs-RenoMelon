b0a22fd791879720d4be6b81ac546a746a135acc
RenoMelon <reno.melon@student.pxl.be>
2025-11-08 12:46:41 +0100

    Lab 2 - Finished
    Added the Customer model and displayed 2 customer objects in the console

--- frontend-web/customer-app/src/app/app.ts
+++ frontend-web/customer-app/src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Customer } from './shared/models/customer.model';
+
+
 
 @Component({
   selector: 'app-root',
@@ -9,4 +12,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('customer-app');
+
+    constructor() {
+    const c1 = new Customer('Dries Swinnen', 'dries@d-ries.be','Pelt','mystreet','Belgium',21);
+    const c2 = new Customer('John Doe', 'john@doe.com','New York','5th Avenue','USA',6, 'john.png');
+    c2.isLoyal = true;
+    
+    console.log(c1);
+    console.log(c2);
+  }
 }
