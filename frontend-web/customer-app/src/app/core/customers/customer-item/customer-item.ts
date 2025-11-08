b52ef41fbefe774a7a25030a1b43bda7389920fb
RenoMelon <reno.melon@student.pxl.be>
2025-11-08 15:24:37 +0100

    Lab 4 - Finished
    Data binding, directives, eventbinding, etc.. added

--- frontend-web/customer-app/src/app/core/customers/customer-item/customer-item.ts
+++ frontend-web/customer-app/src/app/core/customers/customer-item/customer-item.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Customer } from '../../../shared/models/customer.model';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-customer-item',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './customer-item.html',
   styleUrl: './customer-item.css',
 })
 export class CustomerItem {
+  @Input() customer!: Customer;
+
+  getDetails(): void{
+    console.log(this.customer)
+  }
+   
+  
 
 }
+
+
+
+
+
+
+
--- frontend-web/customer-app/src/app/core/customers/customer-list/customer-list.ts
+++ frontend-web/customer-app/src/app/core/customers/customer-list/customer-list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CustomerItem } from '../customer-item/customer-item';
 import { Filter } from '../filter/filter';
+import { Customer } from '../../../shared/models/customer.model';
 
 @Component({
   selector: 'app-customer-list',
@@ -10,4 +11,20 @@ import { Filter } from '../filter/filter';
 })
 export class CustomerList {
 
+  customers!:Customer[]
+
+  ngOnInit(): void{
+    this.customers = [
+       new Customer('Dries Swinnen', 'dries@pxl.be', 'Pelt', 'mystreet', 'Belgium', 21),
+       new Customer('John Doe', 'john@doe.be', 'New York', '5th Avenue', 'USA', 6),
+       new Customer('Jane Doe', 'jane@doe.be', 'Los Angeles', 'Sunset Boulevard', 'USA', 6),
+    ]
+
+    this.customers[1].isLoyal = true;
+  }
+
 }
+
+
+
+
