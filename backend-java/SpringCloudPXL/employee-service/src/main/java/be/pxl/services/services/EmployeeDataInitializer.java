62d8e708dae22a1b945ecb47aa926fdd511d58c2
RenoMelon <reno.melon@student.pxl.be>
2025-11-13 17:21:43 +0100

    Lab 2 - Initial Setup
    Wrote test from the lab video

--- backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/services/EmployeeDataInitializer.java
+++ backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/services/EmployeeDataInitializer.java
@@ -7,12 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Profile("!test")
 public class EmployeeDataInitializer implements ApplicationRunner {
 
     private final EmployeeRepository employeeRepository;
---
+++ backend-java/SpringCloudPXL/employee-service/src/test/java/be/pxl/services/EmployeeTests.java
@@ -0,0 +1,71 @@
+package be.pxl.services;
+
+import be.pxl.services.domain.Employee;
+import be.pxl.services.repository.EmployeeRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+@ActiveProfiles("test") // Dit is gwn voor de DataInitializer niet op te starten als ik tests run
+public class EmployeeTests {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Container
+    private static MySQLContainer sqlContainer = new MySQLContainer("mysql:5.7.37");
+
+    @DynamicPropertySource
+    static void registerMySQLProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", sqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", sqlContainer::getUsername);
+        registry.add("spring.datasource.password", sqlContainer::getPassword);
+    }
+
+    @Test
+    public void testCreateEmployee() throws Exception {
+
+        Employee employee = Employee.builder()
+                .age(22)
+                .name("Roos De Prest")
+                .position("Head of Design")
+                .build();
+
+        String employeeString = objectMapper.writeValueAsString(employee);
+
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/employee")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(employeeString))
+                .andExpect(status().isCreated());
+
+        assertEquals(1, employeeRepository.findAll().size());
+
+    }
+
+
+}
+
