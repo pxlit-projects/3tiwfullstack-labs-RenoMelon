c6a240809ca741b1d0d330c1f00b1d9bbf4f6bb1
Reno Melon <renomeloen@gmail.com>
2025-11-18 12:21:01 +0100

    Lab 3 & 4 - Finished
    New module discovery-service added to implement it as a Eureka Server instance. Also fully configured all other micro-services to be visible to the server by registering them as Eureka Clients.

--- backend-java/SpringCloudPXL/discovery-service/src/main/java/be/pxl/services/DiscoveryServiceApplication.java
+++ backend-java/SpringCloudPXL/discovery-service/src/main/java/be/pxl/services/DiscoveryServiceApplication.java
@@ -2,20 +2,22 @@ package be.pxl.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Hello world!
+ * DiscoveryServiceApplication
  *
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-public class NotificationServiceApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(NotificationServiceApplication.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 }
+
+
 
 
--- backend-java/SpringCloudPXL/organization-service/src/main/java/be/pxl/services/OrganizationServiceApplication.java
+++ backend-java/SpringCloudPXL/organization-service/src/main/java/be/pxl/services/OrganizationServiceApplication.java
@@ -2,6 +2,7 @@ package be.pxl.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrganizationServiceApplication {
 
     public static void main( String[] args )
